39013b5b5726ffc7fde53e7de8444206fdba7b13
Slava Tyhonchuk <slavatyhonchuk@gmail.com>
2020-04-09 19:50:20 +0300

    Idea docs

--- server/modules/user/riskDetection.js
+++ server/modules/user/riskDetection.js
@@ -1,6 +1,10 @@
 const { getConnection } = require('../../db/connect')
 const { searchFeatures } = require('../../db/features')
 
+/**
+ * Checks if the user visited an infected location (or been somewhere near)
+ * and if yes and it was at the time when the virus is still active then mark a user as at risk
+ */
 const figureIfPersonIsAtRisk = async userId => {
   const { db } = await getConnection()
   const options = {

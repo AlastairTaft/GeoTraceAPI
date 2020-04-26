f718c1223aebbe3b06e3e55db3b738ce95d95461
Alastair Taft <ali.c.taft@gmail.com>
2020-04-26 18:03:02 +1000

    Hardened up set infected logic.

--- server/db/users.js
+++ server/db/users.js
@@ -1,41 +1,36 @@
 /**
  * Mark a user as diagnosed with COVID-19
- * @param {MongoDB} db
+ * @param {MongoCollection} db
  * @param {string} uniqueId
+ * @param {object} updateProps
  * @returns {Promise}
  */
-const setUserInfected = async function (
-  db,
+const updateUser = async function (
+  collection,
   uniqueId,
+  updateProps,
 ) {
-  var collection = db.collection('infected')
+  
   return collection.update(
     { uniqueId },
-    { $set: { uniqueId } },
+    { $set: updateProps },
     { upsert: true },
   )
 }
 
 /**
- * Check whether user is infected or not.
+ * Get a user, if the user doesn't exist, it creates a record
  * @param {MongoDB} db
  * @param {string} uniqueId
  * @returns {Promise<boolean>}
  */
-const getUserInfected = async function (db, uniqueId) {
-  var collection = db.collection('infected')
+const getCreateUser = async function (collection, uniqueId) {
   // TODO Update when user has recovered
-  var total = await collection.find({ uniqueId }).count()
-  return total > 0
-}
-
-const notifyRelatedUsersAboutRisk = async features => {
-  // For each received feature but only 1 per user
-  // Generate message that will contain date and readable location when user encountered virus
+  var record = await collection.findOne({ uniqueId })
+  return record
 }
 
 module.exports = {
-  setUserInfected,
-  getUserInfected,
-  notifyRelatedUsersAboutRisk,
+  updateUser,
+  getCreateUser,
 }
--- server/index.js
+++ server/index.js
@@ -2,15 +2,10 @@ var memoize = require('memoizee')
 var { getConnection } = require('./db/connect')
 var { handleServerResponse, rateLimit } = require('./server/server')
 var { ServerError } = require('./server/errors')
-var riskMap = require('./db/riskMap')
 var dbUsers = require('./db/users')
 var serverValidation = require('./server/validation')
 var { hashString } = require('./misc/crypto')
 var dbRiskMap = require('./db/riskMap')
-
-var getUserInfected = memoize(dbUsers.getUserInfected, {
-  normalizer: (db, uniqueId) => uniqueId,
-})
 
 /**
  * Submit risk hashes
@@ -20,12 +15,14 @@ const submitRiskMap = async event => {
   var { uniqueId, hashes } = JSON.parse(event.body)
   var { db, client } = await getConnection()
   var riskMapCollection = db.collection('riskMap')
+  var user = await dbUsers.getCreateUser(db.collection('users'), uniqueId)
   await dbRiskMap.bulkInsert(
     riskMapCollection,
     hashes.map(({ hash, timePassedSinceExposure }) => ({
       uniqueId,
       hash,
       timePassedSinceExposure,
+      infected: user.infected,
     }))
   )
   client.close()
@@ -45,10 +42,10 @@ const reportInfected = async event => {
   serverValidation.validateReportInfectedInput(event)
   var { uniqueId, code } = JSON.parse(event.body)
   var { db, client } = await getConnection()
-  // TODO Validte it is a real code but for testing will allow it through
-  await dbUsers.setUserInfected(db, uniqueId)
+  // TODO Validate it is a real code but for testing will allow it through
+  await dbUsers.updateUser(db.collection('users'), uniqueId, { infected: true })
   await dbRiskMap.markInfectedHashes(db, uniqueId)
-  client.close()
+  await client.close()
   return { 'ok': true }
 }
 
@@ -59,13 +56,14 @@ const getStatus = async event => {
   if (!event.queryStringParameters['unique-id'])
     throw new Error("Missing 'unique-id' prop.")
   var { db, client } = await getConnection()
-  var infected = await dbUsers.getUserInfected(
-    db,
+  var collection = db.collection('users')
+  var user = await dbUsers.getCreateUser(
+    collection,
     event.queryStringParameters['unique-id'],
   )
-  client.close()
+  await client.close()
   return {
-    infected,
+    infected: user.infected,
   }
 }
 
@@ -104,7 +102,7 @@ const getSalt = async event => {
 }
 
 /**
- * Analyse risk.
+ * Analyse risk. To be called periodially on the server
  */
 var analyseRisk = async event => {
 
@@ -118,6 +116,6 @@ module.exports = {
     handleServerResponse(getSalt), 
     Number(process.env.RATE_LIMIT_INTERVAL)
   ),
-  analyseRisk: handleServerResponse(analyseRisk),
+  analyseRisk,
 }
 //1000 * 60 * 60 * 1.5

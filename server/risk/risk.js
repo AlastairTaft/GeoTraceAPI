f8053e5073beefd353cec7f74ac1f6d2240bf5ca
Alastair Taft <ali.c.taft@gmail.com>
2020-05-17 01:51:19 +1000

    WIP adding test to avoid false positive due to timeline of chain of contacts.

--- server/risk/risk.js
+++ server/risk/risk.js
@@ -10,28 +10,42 @@ var areOneOfUsersAtRisk = async function({
   getMatchingHashes,
   chainLength = 1,
 }){
+  //console.log('areOneOfUsersAtRisk#uniqueIds', uniqueIds)
   var userHashes = await getUserHashes(uniqueIds)
+  //console.log('areOneOfUsersAtRisk#userHashes', userHashes)
   var matchingHashes = await getMatchingHashes(userHashes.map(dto => dto.hash))
+  //console.log('areOneOfUsersAtRisk#matchingHashes', matchingHashes)
   // Iterate over the hashes and pull out all the users or see if one of them
   // is marked as infected
   var existingUniqueIds = {}
   uniqueIds.forEach(id => existingUniqueIds[id] = true)
   var allUniqueIds = {}
+  var allUniqueIdsTimeOfInteraction = {}
   var infected = matchingHashes.some(dto => {
     // Don't process it again if we already have so that we don't get stuck in
     // in infinite loop
-    if (!existingUniqueIds[dto.uniqueId])
+    if (!existingUniqueIds[dto.uniqueId]){
       allUniqueIds[dto.uniqueId] = true
+      // Time of interactions
+      allUniqueIdsTimeOfInteraction[dto.uniqueId] = dto.timestamp
+    }
     return dto.infected
   })
   if (infected) return true
   if (chainLength == 1) return false
-  return areOneOfUsersAtRisk({
+  /*return areOneOfUsersAtRisk({
     uniqueIds: Object.keys(allUniqueIds),
     getUserHashes,
     getMatchingHashes,
     chainLength: chainLength -1,
-  })
+  })*/
+  var results = await Promise.all(Object.keys(allUniqueIds).map(uniqueId => areOneOfUsersAtRisk({
+    uniqueIds: [uniqueId],
+    getUserHashes: (uniqueIds) => getUserHashes(uniqueIds, allUniqueIdsTimeOfInteraction[uniqueId]),
+    getMatchingHashes,
+    chainLength: chainLength -1,
+  })))
+  return results.some(r => r)
 }
 
 
--- server/risk/risk.test.js
+++ server/risk/risk.test.js
@@ -1,7 +1,130 @@
 var methods = require('./risk')
 
+/**
+ *  \    / \    /    
+ *  A\ B/  C\ D/
+ *    \/     \/   
+ *    1      2  
+ *    /\     /\
+ *  A/ B\  C/ D\
+ *  /    \ /    \
+ * 3      4      5
+ */
+var timeline1 = []
+
+
 describe('#risk', () => {
   describe('#getAtRiskUsers', () => {
+
+    it(`A is infected and A has interacted with B. After which B interacted 
+      with C so when we want to know who's at risk 2 levels deep we should
+      get B and C.
+        \    / \    /    
+        A\ B/  C\ D/
+          \/     \/   
+          1      2  
+          /\     /\
+        A/ B\  C/ D\
+        /    \ /    \
+       3      4      5
+    `, async () => {
+      
+      var dataPoints = [
+        { hash: '1', uniqueId: 'A', infected: true },
+        { hash: '1', uniqueId: 'B' },
+        { hash: '2', uniqueId: 'C' },
+        { hash: '2', uniqueId: 'D' },
+        { hash: '3', uniqueId: 'A', prevHash: '1', infected: true },
+        { hash: '4', uniqueId: 'B', prevHash: '1' },
+        { hash: '4', uniqueId: 'C', prevHash: '2' },
+        { hash: '5', uniqueId: 'D', prevHash: '2' },
+      ]
+      const getUserHashes = uniqueIds => {
+        if (uniqueIds instanceof Array == false)
+          throw new Error('Expected array for \'uniqueIds\'.')
+        return dataPoints
+          .filter(dto => uniqueIds.includes(dto.uniqueId))
+      }
+      const getMatchingHashes = hashes => {
+        return dataPoints
+          .filter(dto => hashes.some(hash => hash == dto.hash))
+      }
+      var isBAtRisk = await methods.isUserAtRisk({
+        uniqueId: 'B',
+        getUserHashes,
+        getMatchingHashes,
+        chainLength: 2,
+      })
+      expect(isBAtRisk).toBe(true)
+      var isCAtRisk = await methods.isUserAtRisk({
+        uniqueId: 'C',
+        getUserHashes,
+        getMatchingHashes,
+        chainLength: 2,
+      })
+      expect(isCAtRisk).toBe(true)
+      var isDAtRisk = await methods.isUserAtRisk({
+        uniqueId: 'D',
+        getUserHashes,
+        getMatchingHashes,
+        chainLength: 2,
+      })
+      expect(isDAtRisk).toBe(false)
+
+    })
+
+    it(`#foobar A should not be at risk because B interacted with the infected C after
+      A interacted with B.
+        \    / \   
+        A\ B/  C\
+          \/     \    
+          1      2 
+          /\     /
+        A/ B\  C/
+        /    \ /    
+       3      4      
+    `, async () => {
+      var dataPoints = [
+        { hash: '1', uniqueId: 'A', timestamp: 1 },
+        { hash: '1', uniqueId: 'B', timestamp: 1 },
+        { hash: '2', uniqueId: 'C', timestamp: 1, infected: true },
+        { hash: '3', uniqueId: 'A', timestamp: 2 },
+        { hash: '4', uniqueId: 'B', timestamp: 2 },
+        { hash: '4', uniqueId: 'C', timestamp: 2, infected: true },
+      ]
+      const getUserHashes = (uniqueIds, opt_beforeTimestamp) => {
+        if (uniqueIds instanceof Array == false)
+          throw new Error('Expected array for \'uniqueIds\'.')
+        return dataPoints
+          .filter(dto => uniqueIds.includes(dto.uniqueId))
+          .filter(dto => {
+            if (opt_beforeTimestamp) 
+              return dto.timestamp < opt_beforeTimestamp
+            return true
+          })
+      }
+      const getMatchingHashes = hashes => {
+        return dataPoints
+          .filter(dto => hashes.some(hash => hash == dto.hash))
+      }
+      var isAAtRisk = await methods.isUserAtRisk({
+        uniqueId: 'B',
+        getUserHashes,
+        getMatchingHashes,
+        chainLength: 2,
+      })
+      expect(isAAtRisk).toBe(false)
+      var isBAtRisk = await methods.isUserAtRisk({
+        uniqueId: 'B',
+        getUserHashes,
+        getMatchingHashes,
+        chainLength: 2,
+      })
+      expect(isBAtRisk).toBe(true)
+    })
+
+
+
     it('should mark the 1st connection of users at risk', async () => {
       
       var dataPoints = [
@@ -10,9 +133,11 @@ describe('#risk', () => {
         { hash: 'At the supermarket', uniqueId: 'Geoff' },
         { hash: 'At the supermarket', uniqueId: 'Sally' },
       ]
-      const getUserHashes = uniqueId => {
+       const getUserHashes = uniqueIds => {
+        if (uniqueIds instanceof Array == false)
+          throw new Error('Expected array for \'uniqueIds\'.')
         return dataPoints
-          .filter(dto => dto.uniqueId == uniqueId)
+          .filter(dto => uniqueIds.includes(dto.uniqueId))
       }
       const getMatchingHashes = hashes => {
         return dataPoints
@@ -34,7 +159,7 @@ describe('#risk', () => {
       expect(isSallyInfected).toBe(false)
     })
 
-    it(`#foobar should mark the 1st and 2nd in the chain of users as at risk.
+    it(`should mark the 1st and 2nd in the chain of users as at risk.
       Note we don't have any timestamp information so we don't know if
       Sally met Geoff after or before Geoff met Sally. So half the time here
       we may be incorrectly marking Geoff at risk, the more chains we go
@@ -45,9 +170,11 @@ describe('#risk', () => {
         { hash: 'At the supermarket', uniqueId: 'Geoff' },
         { hash: 'At the supermarket', uniqueId: 'Sally' },
       ]
-      const getUserHashes = uniqueId => {
+      const getUserHashes = uniqueIds => {
+        if (uniqueIds instanceof Array == false)
+          throw new Error('Expected array for \'uniqueIds\'.')
         return dataPoints
-          .filter(dto => dto.uniqueId == uniqueId)
+          .filter(dto => uniqueIds.includes(dto.uniqueId))
       }
       const getMatchingHashes = hashes => {
         return dataPoints
